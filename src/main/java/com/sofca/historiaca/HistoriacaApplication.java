9ca0edafa67dc44313ebe26db8c706e56b2f2ed2
Jose Barrios <josealfredobarriossalado@gmail.com>
2024-08-20 11:55:07 -0500

    first commit

---
+++ src/main/java/com/sofca/historiaca/HistoriacaApplication.java
@@ -0,0 +1,13 @@
+package com.sofca.historiaca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoriacaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoriacaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofca/historiaca/HistoriacaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofca.historiaca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoriacaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
