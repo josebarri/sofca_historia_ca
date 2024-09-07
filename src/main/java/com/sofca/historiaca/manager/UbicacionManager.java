54f5eb09bec497ac0bdc991940edf5586c3c15b4
Jose Barrios <josealfredobarriossalado@gmail.com>
2024-09-06 21:55:22 -0500

    mas ajustes

---
+++ src/main/java/com/sofca/historiaca/manager/UbicacionManager.java
@@ -0,0 +1,80 @@
+package com.sofca.historiaca.manager;
+
+import com.sofca.historiaca.dto.MascotaDto;
+import com.sofca.historiaca.dto.UbicacionDto;
+import com.sofca.historiaca.exception.DaoException;
+import com.sofca.historiaca.exception.ManagerException;
+import com.sofca.historiaca.util.crud.CrudDao;
+import com.sofca.historiaca.util.crud.CrudManager;
+
+import java.util.List;
+import java.util.UUID;
+
+public class UbicacionManager implements CrudManager<UbicacionDto> {
+    public CrudDao<UbicacionDto> crudDao;
+
+    public UbicacionManager(CrudDao<UbicacionDto> crudDao) {
+        this.crudDao = crudDao;
+
+    }
+
+    @Override
+    public List<UbicacionDto> selectAll() throws ManagerException {
+        try {
+            return this.crudDao.selectAll();
+
+        } catch (DaoException ex) {
+            throw new ManagerException(ex);
+        } catch (Exception ex) {
+            throw new ManagerException(ex);
+        }
+    }
+
+    @Override
+    public UbicacionDto insert(UbicacionDto ubicacionDto) throws ManagerException {
+        try {
+            UbicacionDto ubicacionDto1 = this.crudDao.insert(ubicacionDto);
+            if (ubicacionDto1 == null) {
+                this.crudDao.insert(ubicacionDto);
+            } else {
+                this.crudDao.update(ubicacionDto);
+            }
+        } catch (DaoException ex) {
+            throw new ManagerException(ex);
+        } catch (Exception ex) {
+            throw new ManagerException(ex);
+        }
+
+        return ubicacionDto;
+    }
+
+
+    @Override
+    public void update(UbicacionDto ubicacionDto) throws ManagerException {
+
+    }
+
+    @Override
+    public UbicacionDto getId(UUID id) throws ManagerException {
+        UbicacionDto ubicacion1 = null;
+        try{
+            ubicacion1 = this.crudDao.getId(id);
+
+        }catch (Exception ex){
+            throw new ManagerException(ex);
+        }
+        return ubicacion1;
+
+    }
+
+    @Override
+    public void deleteId(UUID id) throws ManagerException {
+            try {
+                this.crudDao.deleteId(id);
+            } catch (DaoException ex) {
+                throw new ManagerException(ex);
+            } catch (Exception ex) {
+                throw new ManagerException(ex);
+            }
+        }
+}
